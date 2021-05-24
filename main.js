15504d4d97e8522efd7495fb9b72ff2c5f0a95df
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-05-24 00:37:17 -0500

    Added error checking

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const word = document.getElementById("word");
 const submit = document.getElementById("submit");
 const used = document.getElementById("used");
+const error = document.getElementById("error");
 
 let lastWord = "";
+let previousWords = [];
 
 function check(){
     let checkWord = validWords.find(valid => valid == word.value.toUpperCase());
-    console.log(checkWord)
     let validChange = 0;
+
+    if(!checkWord){
+        showError("The word is invalid");
+        return
+    }
 
 
     Array.from(checkWord).forEach( current => {
@@ -20,13 +26,42 @@ function check(){
     
 
     if( checkWord && lastWord == "" || checkWord && validChange >= lastWord.length - 1){
-        console.log("True!")
+
+        let listCheck = false;
+
+        if(checkWord == lastWord){
+            showError("The word is matches the last word")
+            return;
+        }else{
+            previousWords.forEach(( pWord , i) =>{
+                if(pWord == checkWord){
+                    showError("The word was already used! Game over!")
+                    used.children[i].classList.add("bg-danger")
+                    used.children[i].classList.add("fw-bold")
+                    used.children[i].classList.add("text-white")
+                    listCheck = true
+                }
+            })
+        }
+
+        if(listCheck){
+            return;
+        }
+        
         let item = document.createElement("li");
         item.innerHTML = checkWord;
-        lastWord = checkWord;
+
+        
         item.setAttribute("class","list-group-item")
         used.appendChild(item);
 
+
+        lastWord = checkWord;
+        previousWords.push(checkWord);
+
+        errorReset()
+    }else{
+        showError("The word is invalid")
     }
 }
 
@@ -41,3 +76,13 @@ word.addEventListener("keyup", (e)=>{
     }
     
 })
+
+function showError(string){
+    error.classList.remove("d-none")
+    error.innerHTML = string;
+}
+
+function errorReset(){
+    error.classList.add("d-none")
+    error.innerHTML = '';
+}
